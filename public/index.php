fb3b9f1a1854b5bb796f82aba46238e1cd5e8984
Danar Alifian <danaralifian@gmail.com>
2019-09-21 12:19:39 +0700

    Add GET Products
    read all data products

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../src/config/db.php';
+
+$app = new \Slim\App;
+
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+    return $response;
+});
+
+// Products Routes
+require '../src/routes/products.php';
+
+$app->run();
