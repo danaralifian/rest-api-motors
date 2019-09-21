5db05bfc2c4abb8ea17b7998b061848cf17b7d9a
Danar Alifian <danaralifian@gmail.com>
2019-09-21 14:53:04 +0700

    Add POST Products
    create data product

--- src/routes/products.php
+++ src/routes/products.php
@@ -41,3 +41,45 @@ $app->get('/api/products/{id}', function(Request $request, Response $response){
         echo '{"Error": {"text": '.$e->getMessage().'}';
     }
 });
+
+// GET Single Product
+$app->post('/api/products/add', function(Request $request, Response $response){
+    $productCode = $request->getParam('productCode');
+    $productName = $request->getParam('productName');
+    $productLine = $request->getParam('productLine');
+    $productScale = $request->getParam('productScale');
+    $productVendor = $request->getParam('productVendor');
+    $productDescription = $request->getParam('productDescription');
+    $quantityInStock = $request->getParam('quantityInStock');
+    $buyPrice = $request->getParam('buyPrice');
+    $MSRP = $request->getParam('MSRP');
+
+    $sql = 'INSERT INTO products (productCode,productName,productLine,productScale,productVendor,
+            productDescription,quantityInStock,buyPrice,MSRP) VALUES 
+            (:productCode,:productName,:productLine,:productScale,:productVendor,:productDescription,
+            :quantityInStock,:buyPrice,:MSRP)';
+    try {
+        // GET DB Object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+
+        $stmt = $db->prepare($sql);
+
+        $stmt->bindParam(':productCode', $productCode);
+        $stmt->bindParam(':productName', $productName);
+        $stmt->bindParam(':productLine', $productLine);
+        $stmt->bindParam(':productScale', $productScale);
+        $stmt->bindParam(':productVendor', $productVendor);
+        $stmt->bindParam(':productDescription', $productDescription);  
+        $stmt->bindParam(':quantityInStock', $quantityInStock);
+        $stmt->bindParam(':buyPrice', $buyPrice);
+        $stmt->bindParam(':MSRP', $MSRP);
+
+        $stmt->execute();
+
+        echo '{"message" : {"text" : "Product Added"}';
+    } catch (PDOException $e) {
+        echo '{"Error": {"text": '.$e->getMessage().'}';
+    }
+});
